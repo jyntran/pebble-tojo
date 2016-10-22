72014992ae695b62c5812020e64fda90f803bed5
J. Tran <j.yn.tran@gmail.com>
2016-10-22 15:39:58 -0400

    Added tick handler to tell time, changed font

--- src/c/main.c
+++ src/c/main.c
@@ -11,10 +11,11 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NEUTON_CURSIVE_48));
+
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NEUTON_REGULAR_48));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
@@ -25,13 +26,33 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void init() {
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
   window_stack_push(s_main_window, true);
+  update_time();
 }
 
 static void deinit() {
