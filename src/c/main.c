a2e11cb021d4c6ae8b05081c75aba52d8df2f866
J. Tran <j.yn.tran@gmail.com>
2016-10-22 18:02:03 -0400

    Added logo, moved layers around

--- src/c/main.c
+++ src/c/main.c
@@ -3,40 +3,45 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Image
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TOJO_WHITE_100);
+  s_background_layer = bitmap_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 100));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  
+  // Time
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NEUTON_CURSIVE_48));
-
+      GRect(0, PBL_IF_ROUND_ELSE(8, 0), bounds.size.w, 50));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NEUTON_REGULAR_40));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);  
   text_layer_destroy(s_time_layer);
 }
 
 static void update_time() {
-  // Get a tm structure
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-
-  // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
 }
 
@@ -51,6 +56,7 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  window_set_background_color(s_main_window, GColorBlack);
   window_stack_push(s_main_window, true);
   update_time();
 }
